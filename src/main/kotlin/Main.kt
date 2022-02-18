732a2cbb350424acbaeafaa8da13392e2527ad49
INPARK\2022013 <today8934@interpark.com>
2022-02-18 14:03:32 +0900

    First Kotlin Code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+data class Person(val name: String, val age: Int? = null)
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    val persons = listOf(Person("영희"), Person("철수", age = 29))
+    val oldest = persons.maxByOrNull { it.age ?: 0 }
+    println("나이가 가장 많은 사람: $oldest")
 }
